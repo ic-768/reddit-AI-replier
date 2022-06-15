a0381c0e231d3b8a5aba43470a5e56007656367b
Ioannis Chrysostomakis <ic768@protonmail.com>
2022-06-15 19:30:49 +0300

    Fine-tuning settings and cleanup

--- main.py
+++ main.py
@@ -1,38 +1,55 @@
 from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
 from dotenv import load_dotenv
 import praw
-import re
 import os
 
-load_dotenv('./.env')
+load_dotenv("./.env")
 
 mname = "facebook/blenderbot-400M-distill"
 model = BlenderbotForConditionalGeneration.from_pretrained(mname)
 tokenizer = BlenderbotTokenizer.from_pretrained(mname)
+num_replies = 3
 
 # add these to .env file in project dir
-reddit = praw.Reddit(client_id =os.getenv("REDDIT_CLIENT_ID"),
-        client_secret = os.getenv("REDDIT_CLIENT_SECRET"),
-        user_agent =os.getenv("REDDIT_USER_AGENT"),
-        username=os.getenv("REDDIT_USERNAME"),
-        password=os.getenv("REDDIT_PASSWORD")
-        )
+reddit = praw.Reddit(
+    client_id=os.getenv("REDDIT_CLIENT_ID"),
+    client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
+    user_agent=os.getenv("REDDIT_USER_AGENT"),
+    username=os.getenv("REDDIT_USERNAME"),
+    password=os.getenv("REDDIT_PASSWORD"),
+)
 
-subreddit = reddit.subreddit('all')
+subreddit = reddit.subreddit("all")
 
-# Accept or reject an interaction
-def decide(comment,reply):
-    print("\033[0;31mComment >> ",comment.body)
-    print("\033[0;32mReply >> ",reply,"\033[0m")
-    yesOrNo=input()
-    if yesOrNo.lower() == "y":
-        comment.reply(body=reply)
-        print("posted")
-    else:
-        print('moving on')
 
-def isPromising(text):
-    return text.startswith(
+# User chooses with a number corresponding to a potential reply
+def get_choice():
+    try:
+        choice = int(input())
+        if choice < num_replies and choice > -1:
+            return choice
+        else:
+            raise Exception("Invalid choice")
+    except:
+        print("Rejecting")
+
+
+# Print choice and which number to press to select it
+def print_pretty_choice(idx, choice):
+    print(f"\033[0;32mReply {idx} >> {choice} \033[0m")
+
+
+# Choose or reject interactions
+def decide(comment, replies):
+    print("\033[0;31mComment >> ", comment.body)
+    [print_pretty_choice(i, reply) for i, reply in enumerate(replies)]
+    choice = get_choice()
+    if choice:
+        comment.reply(body=replies[choice])
+
+
+def is_promising(comment):
+    return comment.startswith(
         (
             "what",
             "does",
@@ -46,16 +63,26 @@ def isPromising(text):
     )
 
 
-
 def main():
     for comment in subreddit.stream.comments(skip_existing=True):
-        text=comment.body.lower()
-        if len(text)<90 and not '*' in text and isPromising(text):
-           inputs = tokenizer(comment.body, return_tensors="pt")
-           reply_ids = model.generate(**inputs)
-           reply=tokenizer.batch_decode(reply_ids)[0]
-           formattedReply=re.sub('<.+?>\s?',"",reply)
-           decide(comment,formattedReply)
+        body = comment.body.lower()
+        if len(body) < 90 and not "*" in body and is_promising(body):
+            inputs = tokenizer(
+                comment.body,
+                return_tensors="pt",
+            )
+            replies = model.generate(
+                **inputs,
+                # num_beams=num_replies,
+                # no_repeat_ngram_size=2,
+                # early_stopping=True,
+                do_sample=True,
+                top_k=50,
+                top_p=0.95,
+                num_return_sequences=num_replies,
+            )
+            replies = tokenizer.batch_decode(replies, skip_special_tokens=True)
+            decide(comment, replies)
 
 
 main()
