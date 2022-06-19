8ead2ca792df51de318176614e1c9af18596fa55
Ioannis Chrysostomakis <ic768@protonmail.com>
2022-06-19 18:43:22 +0300

    Can respond to replies

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
 from dotenv import load_dotenv
 from time import sleep
 import praw
+from praw.models import Comment
 import os
 
 load_dotenv("./.env")
@@ -38,26 +39,73 @@ def is_promising(comment):
     )
 
 
+def generate_response(input):
+    inputs = tokenizer(
+        input,
+        return_tensors="pt",
+    )
+    reply = model.generate(
+        **inputs,
+        # num_beams=num_replies,
+        # no_repeat_ngram_size=2,
+        # early_stopping=True,
+        do_sample=True,
+        top_k=50,
+        top_p=0.95,
+    )
+    return tokenizer.batch_decode(reply, skip_special_tokens=True)[0]
+
+
+def should_reply(text):
+    return len(text) < 90 and len(text) > 15 and not "*" in text and is_promising(text)
+
+
+def is_mod_comment(text):
+    return "am a bot" in text or "moderators" in text
+
+
+#def main():
+#    for comment in subreddit.stream.comments(skip_existing=True):
+#        body = comment.body.lower()
+#        if should_reply(body) and not is_mod_comment(body):
+#            reply = generate_response(comment.body)
+#            print("replying", reply)
+#            try:
+#                comment.reply(body=reply)
+#                sleep(200)
+#            except Exception as e:
+#                print(e)
+#
+#
+#main()
+
+
 def main():
-    for comment in subreddit.stream.comments(skip_existing=True):
-        body = comment.body.lower()
-        if len(body) < 90 and not "*" in body and is_promising(body):
-            inputs = tokenizer(
-                comment.body,
-                return_tensors="pt",
-            )
-            reply = model.generate(
-                **inputs,
-                # num_beams=num_replies,
-                # no_repeat_ngram_size=2,
-                # early_stopping=True,
-                do_sample=True,
-                top_k=50,
-                top_p=0.95,
-            )
-            reply = tokenizer.batch_decode(reply, skip_special_tokens=True)[0]
-            comment.reply(body=reply)
-            sleep(220)
+   for comment in subreddit.stream.comments(skip_existing=True, pause_after=-1):
+       if comment is None:
+           for i in reddit.inbox.unread(limit=None):
+               if isinstance(
+                   i, Comment
+               ):  # make sure it's a comment, because it could also be a message
+                   reply = generate_response(i.body)
+                   try:
+                       if not is_mod_comment(i):
+                           i.reply(body=reply)
+                           i.mark_read()
+                           print("replying to reply")
+                           sleep(220)
+                   except Exception as e:
+                       print("exception in reply", e)
+       else:
+           body = comment.body.lower()
+           if should_reply(body) and not is_mod_comment(body):
+               reply = generate_response(comment.body)
+               try:
+                   comment.reply(body=reply)
+                   print("replying to comment")
+                   sleep(220)
+               except Exception as e:
+                   print("exception in reply", e)
 
 
 main()
